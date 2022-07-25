f7a6350aa369bd7d5652aac4e1a91ea0f6d74c37
shahrierhuq <shahrierhuq@gmail.com>
2022-07-24 20:37:46 -0400

    new features

---
+++ flask_api/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/hello/', methods=['GET', 'POST'])
+def welcome():
+    return "Hello World!"
+
+@app.route('/<string:name>')
+def hello(name):
+    return "Hello " + name
+           
+@app.route('/<int:number>/')
+def incrementer(number):
+    return "Incremented number is " + str(number+1)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=105)
