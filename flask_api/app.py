1eef5b079c5ace9db9b8714fe4824c0394ae7cd5
shahrierhuq <shahrierhuq@gmail.com>
2022-07-24 21:49:21 -0400

    json addition

--- flask_api/app.py
+++ flask_api/app.py
@@ -1,17 +1,22 @@
 from flask import Flask
+from flask import jsonify
 app = Flask(__name__)
 
-@app.route('/hello/', methods=['GET', 'POST'])
+@app.route('/home/', methods=['GET', 'POST'])
 def welcome():
-    return "Hello World!"
+    return "Hello, welcome to the home page."
 
-@app.route('/<string:name>/')
+@app.route('/home/<string:name>/')
 def hello(name):
     return "Hello " + name
            
 @app.route('/<int:number>/')
 def incrementer(number):
     return "Incremented number is " + str(number+1)
+
+@app.route('/json_test/')
+def print_list():
+    return jsonify(list(range(10)))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=105)
